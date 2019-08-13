d226449bd0fb39b66d27a398237842ca5d8e5f88
mohamamad <mohamad.shahrokhi@gmail.com>
2019-08-13 17:54:00 +0430

    debugging

--- src/app/Classes/AdPicTransferrer.php
+++ src/app/Classes/AdPicTransferrer.php
@@ -8,7 +8,6 @@ use App\Traits\HTTPRequestTrait;
 use Exception;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class AdPicTransferrer
