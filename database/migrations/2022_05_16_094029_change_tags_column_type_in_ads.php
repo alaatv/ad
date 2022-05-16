64def4e3710495471cbfbbf336deedc5148d2fef
salar <salarsabeti88@gmail.com>
2022-05-16 14:21:24 +0430

    change tags column type to text in ads table to prevent error : Data too long for column 'tags'

---
+++ database/migrations/2022_05_16_094029_change_tags_column_type_in_ads.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeTagsColumnTypeInAds extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ads', function (Blueprint $table) {
+            $table->text('tags')->nullable()->comment('تگ های تبلیغ')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ads', function (Blueprint $table) {
+            //
+        });
+    }
+}
