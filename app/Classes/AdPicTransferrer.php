f0b3e115dcfbbeca9b944e98856d79810ea803d1
salar <salarsabeti88@gmail.com>
2022-05-17 11:30:24 +0430

    make ad image to store correctly and fill image column in ads table

--- app/Classes/AdPicTransferrer.php
+++ app/Classes/AdPicTransferrer.php
@@ -6,9 +6,9 @@ namespace App\Classes;
 
 use App\Traits\HTTPRequestTrait;
 use Exception;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdPicTransferrer
 {
@@ -18,22 +18,22 @@ class AdPicTransferrer
      * @param string $picUrl
      * @return array
      */
-    public function storeAdPic(string $picUrl=null): array
+    public function storeAdPic(string $picUrl = null): array
     {
-        try{
-            if(!isset($picUrl))
+        try {
+            if (!isset($picUrl))
                 return [false, null];
 
-            $pathToSave = Storage::disk('adImage')->getAdapter()->getPathPrefix() . basename($picUrl);
-            $filePath = fopen($pathToSave, 'w');
+            $pathToSave = Storage::disk('adImage')->putFileAs('', $picUrl, basename($picUrl));
+            $filePath = fopen(storage_path('app/public/images/ads/') . $pathToSave, 'w+');
 
-            $response = $this->sendRequest($picUrl, 'GET', null, null , $filePath);
-            if($response['statusCode'] == Response::HTTP_OK){
-                return [true,$pathToSave];
+            $response = $this->sendRequest($picUrl, 'GET', null, null, $filePath);
+            if ($response['statusCode'] == Response::HTTP_OK) {
+                return [true, 'app/public/images/ads/' . $pathToSave];
             }
             return [false, null];
 
-        } catch ( Exception $e ) {
+        } catch (Exception $e) {
             return [false, null];
         }
     }
@@ -42,13 +42,16 @@ class AdPicTransferrer
      * @param string $filePath
      * @return
      */
-    public function transferAdPicToCDN(string $filePath):array {
+    public function transferAdPicToCDN(string $filePath): array
+    {
         $disk = Storage::disk('adPicsSFTP');
         $fileName = basename($filePath);
         $url = null;
         $done = false;
-        if ($disk->put($fileName, File::get($filePath))) {
-            $url = config('download_server.IMAGES_PARTIAL_PATH'). '/'.$fileName;
+
+        //TODO : login to filesystem in production ENV (set correct username and host)
+        if ($disk->put($fileName, File::get(storage_path($filePath)))) {
+            $url = config('download_server.IMAGES_PARTIAL_PATH') . '/' . $fileName;
             $done = true;
             Storage::disk('adImage')->delete($fileName);
         }
